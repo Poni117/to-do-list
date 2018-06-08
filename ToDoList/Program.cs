ef52f72880e22472c8106bd81df916f8e8dc4e55
Poni117 <Poni117@mail.ru>
2018-06-08 23:18:26 +0300

    Add Task & User can enter tasks

---
+++ ToDoList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько задач, господин");
+            int taskNumber = Int32.Parse(Console.ReadLine());
+            Task[] tasks = new Task[taskNumber];
+
+            for (int i = 0; i < taskNumber; i++)
+            {
+                Console.WriteLine("Введите задачу");
+                string userAction = Console.ReadLine();
+                tasks[i] = new Task(i, userAction);
+            }
+
+
+           for (int i = 0; i < tasks.Length - 1; i++)
+           {
+                Console.WriteLine($"{tasks[i].GetId()}. {tasks[i].GetAction()}");
+           }
+        }
+
+    }
+}
---
+++ ToDoList/Task.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class Task
+    {
+        private int _id;
+        private string _action;
+
+        public Task(int id, string action)
+        {
+            _id = id;
+            _action = action;
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+        public string GetAction()
+        {
+            return _action;
+        }
+    }
+}
